dee777ab8fe06bec34c55a45f961c1d366605b82
tc-imba <liuyh970615@gmail.com>
2023-11-22 01:16:08 +0800

    fix tags in config.js

--- config.js
+++ config.js
@@ -197,52 +197,60 @@ module.exports = {
     'magenta', 'red', 'volcano', 'orange', 'gold',
     'lime', 'green', 'cyan', 'blue', 'geekblue', 'purple',
   ],
-  tags: {
-    javascript: {
+  tags: [
+    {
+      id: 'javascript',
       name: 'javascript',
       description: 'JavaScript is an object-oriented programming language used alongside HTML and CSS to give functionality to web pages.',
       color: '#f0da50',
     },
-    nodejs: {
+    {
+      id: 'nodejs',
       name: 'Node.js',
       description: 'Node.js is a tool for executing JavaScript in a variety of environments.',
       color: '#90c53f',
     },
-    rxjs: {
+    {
+      id: 'rxjs',
       name: 'RxJS',
       description: 'RxJS is a library for reactive programming using Observables, for asynchronous operations.',
       color: '#eb428e',
     },
-    typescript: {
+    {
+      id: 'typescript',
       name: 'typescript',
       description: 'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.',
       color: '#257acc',
     },
-    reactjs: {
+    {
+      id: 'reactjs',
       name: 'reactjs',
       description: 'React is an open source JavaScript library used for designing user interfaces.',
       color: '#61dbfa',
     },
-    gatsby: {
+    {
+      id: 'gatsby',
       name: 'Gatsby.js',
       description: 'A framework built over ReactJS to generate static page web application.  ',
       color: '#6f309f',
     },
-    html: {
+    {
+      id: 'html',
       name: 'HTML',
       description: 'A markup language that powers the web. All websites use HTML for structuring the content.',
       color: '#dd3431',
     },
-    css: {
+    {
+      id: 'css',
       name: 'css',
       description: 'CSS is used to style the HTML element and to give a very fancy look for the web application.',
       color: '#43ace0',
     },
-    python: {
+    {
+      id: 'python',
       name: 'python',
       description: 'A general purpose programming language that is widely used for developing various applications.',
       color: '#f9c646',
     },
-
-  },
+  ],
 };
